74e20864555f46baddf2ce7818db977441684fad
Mefiso <polpegra@gmail.com>
2020-10-22 18:07:41 +0200

    finished exercise with including cpp

--- 01_Vector3/01_Vector3/main.cpp
+++ 01_Vector3/01_Vector3/main.cpp
@@ -1,12 +1,46 @@
-#include <iostream>
+﻿#include <iostream>
 #include "vector3.h"
+#include "vector3.cpp"
 
 template<class T>
 void print_vec(const Vec3<T>& v, std::string name);
 
 int main(int argc, char* argv[])
 {
-	Vec3<int> a(1, 0, 1);
+	Vec3<int> a;
+
+	a.set_x(1);
+	a.set_y(3);
+	a.set_z(1);
+	print_vec(a, "a");
+	std::cout << a.length() << std::endl;
+
+	Vec3<int> b(1, 0, 1);
+	print_vec(b, "b");
+	std::cout << b.length() << std::endl;
+
+	std::cout << "a·b = " << a.dot_product(b) << std::endl;
+
+	print_vec(a.cross_product(b), "a x b");
+	print_vec(b.cross_product(a), "b x a");
+
+	std::cout << "Angle between a and b: " << a.angle_between(b) << std::endl;
+
+	Vec3<float> c(1.0f, 2.0f, 3.0f);
+	Vec3<float> d(c);
+	
+	print_vec(c, "c");
+	print_vec(d, "d");
+
+	std::cout << d.length() << std::endl;
+	d.Normalize();
+	std::cout << "\nNormalized" << std::endl;
+	print_vec(d, "d normalized");
+	std::cout << d.length() << std::endl;
+
+	Vec3<int> e = a + b;
+	print_vec(e, "e");
+
 
 }
 
--- 01_Vector3/01_Vector3/vector3.cpp
+++ 01_Vector3/01_Vector3/vector3.cpp
@@ -1,4 +1,5 @@
 #include <math.h>
+#include<tuple>
 #include "vector3.h"
 
 template <class T>
@@ -29,15 +30,15 @@ Vec3<T>::~Vec3()
 template<class T>
 void Vec3<T>::Normalize()
 {
-	double l = this->length;
+	double l = this->length();
 	for (auto it = this->components.begin(); it != this->components.end(); ++it)
-		(*it) = (*it) / length;
+		(*it) = (*it) / l;
 }
 
 template<class T>
 double Vec3<T>::distance_to(const Vec3<T>& v) const
 {
-	T diff[3] = { v.x, v.y, v.z};
+	T diff[3] = { v.x(), v.y(), v.z()};
 	double sum = 0.;
 	int i = 0;
 	
@@ -53,7 +54,7 @@ double Vec3<T>::distance_to(const Vec3<T>& v) const
 template<class T>
 T Vec3<T>::dot_product(const Vec3<T>& v) const
 {
-	T diff[3] = { v.x, v.y, v.z };
+	T diff[3] = { v.x(), v.y(), v.z() };
 	T result = 0;
 	int i = 0;
 	for (auto it = this->components.begin(); it != this->components.end(); ++it) {
@@ -67,9 +68,9 @@ T Vec3<T>::dot_product(const Vec3<T>& v) const
 template<class T>
 Vec3<T> Vec3<T>::cross_product(const Vec3<T>& v) const
 {
-	return Vec3<T>(this->y * v.z - this->z * v.y,
-					this->z * v.x - this->x * v.z,
-					this->x * v.y - this->y * v.x);
+	return Vec3<T>(this->y() * v.z() - this->z() * v.y(),
+					this->z() * v.x() - this->x() * v.z(),
+					this->x() * v.y() - this->y() * v.x());
 }
 
 template<class T>
@@ -78,11 +79,21 @@ double Vec3<T>::angle_between(const Vec3<T>& v) const
 	const T dot_p = this->dot_product(v);
 	const double cosinus = dot_p / (this->length() * v.length());
 	
-	return acos(cosinus);
+	return (180*acos(cosinus)/ 3.1416);
 }
 
 template<class T>
 inline double Vec3<T>::length() const
 {
-	return sqrt((double) this->dot_product(this));
+	return sqrt((double) this->dot_product(*this));
+}
+
+template<class T>
+Vec3<T> Vec3<T>::operator+(const Vec3<T>& v)
+{
+	T sum[3] = { v.x(), v.y(), v.z() };
+	for (auto [it, i] = std::tuple{ this->components.begin(), 0 }; it != this->components.end(); ++it, ++i)
+		sum[i] += (*it);
+
+	return Vec3<T>(sum[0], sum[1], sum[2]);
 }
